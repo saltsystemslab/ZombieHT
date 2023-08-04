d4e07e52a078c6ee3b198042e083b2dfe16e0727
benwei <b.shi@utah.edu>
2023-08-04 10:30:01 -0600

    Remove rhm debug code

--- include/rhm_wrapper.h
+++ include/rhm_wrapper.h
@@ -13,25 +13,16 @@ extern inline int g_rhm_init(uint64_t nslots, uint64_t key_size, uint64_t value_
 
 extern inline int g_rhm_insert(uint64_t key, uint64_t val)
 {
-	#if DEBUG
-	printf("Insert key:%lx %lx\n", key, val);
-	#endif
 	return rhm_insert(&g_robinhood_hashmap, key, val, QF_NO_LOCK | QF_KEY_IS_HASH);
 }
 
 extern inline int g_rhm_lookup(uint64_t key, uint64_t *val)
 {
-	#if DEBUG
-	printf("Lookup key:%lx\n", key);
-	#endif
 	return rhm_lookup(&g_robinhood_hashmap, key, val, QF_NO_LOCK | QF_KEY_IS_HASH);
 }
 
 extern inline int g_rhm_remove(uint64_t key)
 {
-	#if DEBUG
-	printf("Remove key:%lx\n", key);
-	#endif
 	return rhm_remove(&g_robinhood_hashmap, key, QF_NO_LOCK | QF_KEY_IS_HASH);
 }
 
