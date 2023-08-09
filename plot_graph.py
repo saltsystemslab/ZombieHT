db25a1bed66895b77cec7a2f7955d3d13b209e83
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-08-09 23:01:16 +0000

    Plot graph script

--- plot_graph.py
+++ plot_graph.py
@@ -3,21 +3,23 @@ import sys
 import pandas as pd
 from matplotlib import pyplot as plt
 
-num_keys = sys.argv[1]
-num_churn_ops = sys.argv[2]
 
-print(num_keys, num_churn_ops)
-
-df = pd.read_csv('rhm-insert.txt', delim_whitespace=True)
-plt.plot(df["x_0"], df["y_0"])
-plt.xlabel("percent of keys inserted (%d)" % int(num_keys))
-plt.ylabel("throughput (num inserts per sec)")
-plt.savefig("plot_insert.png")
+df_rhm = pd.read_csv('rhm-thrput-load.txt.txt', delim_whitespace=True)
+df_trhm = pd.read_csv('trhm-thrput-load.txt.txt', delim_whitespace=True)
+plt.xlabel("inital_load_%")
+plt.ylabel("ops/usec")
+plt.plot(df_rhm["x_0"], df_rhm["y_0"], label="rhm")
+plt.plot(df_trhm["x_0"], df_trhm["y_0"], label="trhm")
+plt.legend()
+plt.savefig("plot_load.png")
 plt.close()
 
-df = pd.read_csv('rhm-churn.txt', delim_whitespace=True)
-plt.plot(df["x_0"], df["y_0"])
-plt.xlabel("percent of churn test(%d)" % int(num_churn_ops))
-plt.ylabel("throughput (num ops per sec)")
+df_rhm = pd.read_csv('rhm-thrput-churn.txt.txt', delim_whitespace=True)
+df_trhm = pd.read_csv('trhm-thrput-churn.txt.txt', delim_whitespace=True)
+plt.xlabel("churn_%")
+plt.ylabel("ops/usec")
+plt.plot(df_rhm["x_0"], df_rhm["y_0"], label="rhm")
+plt.plot(df_trhm["x_0"], df_trhm["y_0"], label="trhm")
+plt.legend()
 plt.savefig("plot_churn.png")
 plt.close()
