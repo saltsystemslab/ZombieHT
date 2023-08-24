52c764325ac636313776c515ebd2218cd036fce6
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-08-24 21:15:24 +0000

    Add deamortized graveyard hashing

--- bench/run_churn.sh
+++ bench/run_churn.sh
@@ -9,4 +9,7 @@ make clean hm_churn T=1 REBUILD_BY_CLEAR=1
 make clean hm_churn T=1 REBUILD_AMORTIZED_GRAVEYARD=1
 ./hm_churn -k 30 -q 24 -c 20 -l 1000 -d ./bench_run/grhm/
 
+make clean hm_churn T=1 REBUILD_DEAMORTIZED_GRAVEYARD=1
+./hm_churn -k 30 -q 24 -c 20 -l 1000 -d ./bench_run/gzhm/
+
 ./bench/plot_graph.py
