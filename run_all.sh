e73085008d5a416aabb8b7cac385a77faded0e41
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-10-09 23:10:31 +0000

    submodule commits

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./bench/paper/gzhm_external_latency.sh
-./bench/paper/gzhm_variants_latency.sh
-./bench/paper/gzhm_external.sh
-./bench/paper/gzhm_variants.sh
+#./bench/paper/gzhm_external.sh 2 0
+#./bench/paper/gzhm_external.sh 2 1
+# ./bench/paper/gzhm_variants.sh 2 0
+#./bench/paper/gzhm_variants.sh 2 1
+./bench/paper/gzhm_pts_study.sh 2 0
+./bench/paper/gzhm_delete_study.sh 2 0
+
